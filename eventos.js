75b637a4dbcad0d62a8d606a6cb8924556b1c638
ValeGaricano <137462184+ValeGaricano@users.noreply.github.com>
2023-09-03 17:26:12 -0300

    probando ultima solucion

--- eventos.js
+++ eventos.js
@@ -1,10 +1,8 @@
 let div = document.getElementById('div');
 
-function mensajeDiv(e){
+div.addEventListener('click', function(){
     alert("Hola! Soy el div");
-}
-
-div.addEventListener('click', mensajeDiv);
+});
 
 /*div.addEventListener('click', function(){
     parrafo.innerHTML = alert("Hola! Soy el div");
